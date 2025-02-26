994d57a95f8f9f0adae15e79fa84ceeda8ac7981
avernikoz <avernikoz@gmail.com>
2025-02-26 16:35:45 +0300

    fix withdraw charged fee script

--- examples/wrapper/withdraw-charged-fee.ts
+++ examples/wrapper/withdraw-charged-fee.ts
@@ -1,5 +1,5 @@
 import { Transaction } from "@mysten/sui/transactions";
-import { keypair, provider } from "../common";
+import { keypair, provider, user } from "../common";
 import { FUND_CUP_OBJECT_ID, NS_COIN_TYPE, WRAPPER_OBJECT_ID, WRAPPER_PACKAGE_ID } from "../constants";
 
 const FEE_COIN_TYPE = NS_COIN_TYPE;
@@ -8,11 +8,13 @@ const FEE_COIN_TYPE = NS_COIN_TYPE;
 (async () => {
   const tx = new Transaction();
 
-  tx.moveCall({
+  const withdrawnCoin = tx.moveCall({
     target: `${WRAPPER_PACKAGE_ID}::wrapper::withdraw_charged_fee`,
     typeArguments: [FEE_COIN_TYPE],
     arguments: [tx.object(FUND_CUP_OBJECT_ID), tx.object(WRAPPER_OBJECT_ID)],
   });
+
+  tx.transferObjects([withdrawnCoin], tx.pure.address(user));
 
   const res = await provider.signAndExecuteTransaction({ transaction: tx, signer: keypair });
 
