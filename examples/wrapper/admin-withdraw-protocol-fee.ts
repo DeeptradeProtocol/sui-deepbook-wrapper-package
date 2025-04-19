45f23bd779c9f60b574bb9e41a9bac9b4361ad2c
bathord <bathord43@gmail.com>
2025-04-19 16:13:41 +0300

    Add DEEP withdrawal from protocol fee withdraw example

--- examples/wrapper/admin-withdraw-protocol-fee.ts
+++ examples/wrapper/admin-withdraw-protocol-fee.ts
@@ -1,14 +1,24 @@
 import { keypair, provider, user } from "../common";
-import { ADMIN_CAP_OBJECT_ID, SUI_COIN_TYPE, WRAPPER_PACKAGE_ID } from "../constants";
+import { ADMIN_CAP_OBJECT_ID, DEEP_COIN_TYPE, SUI_COIN_TYPE, WRAPPER_PACKAGE_ID } from "../constants";
 import { getWithdrawFeeTx } from "./getWithdrawFeeTx";
 
 // yarn ts-node examples/wrapper/admin-withdraw-protocol-fee.ts > admin-withdraw-protocol-fee.log 2>&1
 (async () => {
+  // Withdraw SUI protocol fee
   const tx = getWithdrawFeeTx({
     coinType: SUI_COIN_TYPE,
     target: `${WRAPPER_PACKAGE_ID}::wrapper::admin_withdraw_protocol_fee`,
     user,
     adminCapId: ADMIN_CAP_OBJECT_ID,
+  });
+
+  // Withdraw DEEP protocol fee (pool creation fee)
+  getWithdrawFeeTx({
+    coinType: DEEP_COIN_TYPE,
+    target: `${WRAPPER_PACKAGE_ID}::wrapper::admin_withdraw_protocol_fee`,
+    user,
+    adminCapId: ADMIN_CAP_OBJECT_ID,
+    transaction: tx,
   });
 
   const res = await provider.signAndExecuteTransaction({ transaction: tx, signer: keypair });
