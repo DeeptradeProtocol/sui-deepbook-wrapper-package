69674e87d726c5627de2ae8e09445c2601f2b171
avernikoz <avernikoz@gmail.com>
2025-04-19 15:44:52 +0300

    add get-deep-reserves-balance-info

--- examples/wrapper/withdraw-all-deep-reserves.ts
+++ examples/wrapper/withdraw-all-deep-reserves.ts
@@ -1,22 +1,22 @@
 import { Transaction } from "@mysten/sui/transactions";
 import { keypair, provider, user } from "../common";
 import { ADMIN_CAP_OBJECT_ID, DEEP_DECIMALS, WRAPPER_OBJECT_ID, WRAPPER_PACKAGE_ID } from "../constants";
-
-const WITHDRAW_AMOUNT = 1; // 1 DEEP
+import { getDeepReservesBalance } from "./utils/getDeepReservesBalance";
 
 // yarn ts-node examples/wrapper/withdraw-deep-reserves.ts > withdraw-deep-reserves.log 2>&1
 (async () => {
   const tx = new Transaction();
 
-  const rawAmount = WITHDRAW_AMOUNT * 10 ** DEEP_DECIMALS;
+  const { deepReservesRaw: amountToWithdraw, deepReserves: amountToWithdrawFormatted } = await getDeepReservesBalance();
 
   const withdrawnCoin = tx.moveCall({
     target: `${WRAPPER_PACKAGE_ID}::wrapper::withdraw_deep_reserves`,
-    arguments: [tx.object(ADMIN_CAP_OBJECT_ID), tx.object(WRAPPER_OBJECT_ID), tx.pure.u64(rawAmount)],
+    arguments: [tx.object(ADMIN_CAP_OBJECT_ID), tx.object(WRAPPER_OBJECT_ID), tx.pure.u64(amountToWithdraw)],
   });
 
   tx.transferObjects([withdrawnCoin], tx.pure.address(user));
 
+  console.warn(`Withdrawing ${amountToWithdrawFormatted} DEEP`);
   const res = await provider.signAndExecuteTransaction({ transaction: tx, signer: keypair });
 
   console.log(res);
