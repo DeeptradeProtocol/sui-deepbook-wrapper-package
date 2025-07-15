927095fc99c900cb141dcbe33b9015c5534d1c2a
avernikoz <avernikoz@gmail.com>
2025-07-15 23:03:29 +0300

    remove hardcoded deep decimals

--- examples/pool/update-pool-creation-protocol-fee.ts
+++ examples/pool/update-pool-creation-protocol-fee.ts
@@ -1,14 +1,14 @@
 import { Transaction } from "@mysten/sui/transactions";
 import { provider } from "../common";
-import { ADMIN_CAP_OBJECT_ID, POOL_CREATION_CONFIG_OBJECT_ID, WRAPPER_PACKAGE_ID } from "../constants";
+import { ADMIN_CAP_OBJECT_ID, DEEP_DECIMALS, POOL_CREATION_CONFIG_OBJECT_ID, WRAPPER_PACKAGE_ID } from "../constants";
 import { MULTISIG_CONFIG } from "../multisig";
 
 // Set this value to the amount you want to set the new fee to
-const NEW_FEE = 200 * 1_000_000; // 200 DEEP
+const NEW_FEE = 200 * 10 ** DEEP_DECIMALS; // 200 DEEP
 
 // yarn ts-node examples/pool/update-pool-creation-protocol-fee.ts
 (async () => {
-  console.warn(`Building transaction to update pool creation protocol fee to ${NEW_FEE / 1_000_000} DEEP`);
+  console.warn(`Building transaction to update pool creation protocol fee to ${NEW_FEE / 10 ** DEEP_DECIMALS} DEEP`);
 
   const tx = new Transaction();
 
